42d4909bc913960af924628db659d0ff14c5e44b
Edwin Fuquen <efuquen@gmail.com>
2016-02-13 15:42:39 -0500

    listen in to map events

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,25 @@
 window.onload = function() {
-  cartodb.createVis('map', 'https://efuquen.cartodb.com/api/v2/viz/dd5312ae-d222-11e5-999d-0e787de82d45/viz.json');
+	var visUrl = 'https://efuquen.cartodb.com/api/v2/viz/dd5312ae-d222-11e5-999d-0e787de82d45/viz.json';
+  cartodb.createVis('map', visUrl).done(function(vis) {
+		console.log(vis);
+		var layers = vis.getLayers();
+		console.log(layers);
+		for (var i = 0; i < layers.length; i ++) {
+			var layer = layers[i];
+			//layer.set();
+			//layer.set({'interactivity': 'precinct, complaints'});
+			//var sublayer = layer.getSubLayer(0);
+			if ('getSubLayer' in layer) {
+				layer.getSubLayer(0).set({'interactivity': 'precinct, complaints'});
+			}
+			//console.log(layer);
+			layer.on('featureClick', function(e, latlng, pos, data, subLayerIndex) {
+				if ('precinct' in data) {
+					//$('#precinct').
+				}
+			});
+		}
+	});
 }
 $(function(){
 
